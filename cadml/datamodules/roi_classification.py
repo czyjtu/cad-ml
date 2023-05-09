84eb45552033e66b71e5459be1a35231d5d14842
czyjtu <maciejczyjtu@gmail.com>
2023-05-09 18:27:49 +0200

    repalce handcrafted transformations with torchvision transforms; update requirements

--- cadml/datamodules/roi_classification.py
+++ cadml/datamodules/roi_classification.py
@@ -7,138 +7,87 @@ from omegaconf import DictConfig
 import torch
 from torch.utils.data import DataLoader, Dataset
 from torchtyping import TensorType
-
+import torch as th
+from torchvision.transforms import (
+    Compose,
+    RandomHorizontalFlip,
+    RandomVerticalFlip,
+    RandomRotation,
+    ToTensor,
+)
 import pytorch_lightning as pl
 from pytorch_lightning.utilities.types import TRAIN_DATALOADERS, EVAL_DATALOADERS
-from pytorch_lightning.utilities import rank_zero_info
 
 import coronaryx as cx
 import coronaryx.algorithms as cxa
 import coronaryx.functions as cxf
+import matplotlib.pyplot as plt
 
 
 class ROIClassificationDataset(Dataset):
     def __init__(
-            self,
-            dataset: list[cx.VesselBranch],
-            size: int = 32,
-            required_positive_overlap: float = 0.5,  # FIXME not used currently! do we need it? is anchor enough?
-            upsample_positive_examples: bool = False,
-            train_transform: bool = True,
-            transform_size_mean: float = 0.0,
-            transform_size_std: float = 3.0,
+        self,
+        dataset: list[cx.VesselBranch],
+        transforms: Optional[Any] = None,
+        size: int = 32,
+        upsample_positive_examples: bool = False,
     ):
         """
         PyTorch Dataset based on the angiograms, extracting images along the vessels, labeling stenosis,
         and applying data augmentation.
 
         :param dataset: list of vessel branches objects
+        :param transforms: torchvision transforms to be applied to the images
         :param size: size of the output images
-        :param required_positive_overlap: minimum overlap with positive ROI required to be considered a positive example
         :param upsample_positive_examples: upsample the number of positive examples to the number of negative ones
-        :param train_transform: apply data augmentation techniques
-        :param transform_size_mean: mean of generated eps (we cut out picture of size `size + eps` from the original
-                                                            and then resize it to `size`)
-        :param transform_size_std: std generated eps (we cut out picture of size `size + eps` from the original
-                                                            and then resize it to `size`)
         """
 
         self.original_dataset = dataset
-        self.train_transform = train_transform
-
         self.size = size
-
-        self.transform_size_mean = transform_size_mean
-        self.transform_size_std = transform_size_std
-
-        self.dataset = [
-            {
-                'branch_obj': branch,  # VesselBranch
-                'anchor': anchor,  # np.ndarray['2', float]
-                'angle': 0.0,  # float
-                'size': size,  # int
-                'mirrored': False,  # bool
-                # TODO another transformations? background tweaking? noise?
-            }
-            for branch in dataset
-            for anchor in cxa.traverse_branch_nodes(branch)
-        ]
-
         self.upsample_positive_examples = upsample_positive_examples
-        self.positive_examples = [
-            anchor
-            for anchor in self.dataset
-            if anchor['branch_obj'].scan.in_any_roi(anchor['anchor'])
-        ]
-        self.negative_examples = [
-            anchor
-            for anchor in self.dataset
-            if not anchor['branch_obj'].scan.in_any_roi(anchor['anchor'])
-        ]
 
-    def _transform(self, item: dict[str, Any]) -> dict[str, Any]:
-        return {
-            'branch_obj': item['branch_obj'],
-            'anchor': item['anchor'],
-            'angle': np.random.uniform(0, 2 * np.pi),  # TODO what if the angle will always match the angle of vector (vessel will lay horizontally)?
-            'size': item['size'] + self.transform_size_mean + self.transform_size_std * np.random.randn(),
-            'mirrored': np.random.random() < 0.5,
-        }
-
-    def _materialize_item(self, item: dict[str, Any]) -> tuple[TensorType["in_channels", "height", "width"], int]:
-        scan: cx.CoronagraphyScan = item['branch_obj'].scan
-        image = np.copy(scan.scan) / 255.0  # normalizing
-
-        # creating meshgrid of points (corresponding with each pixel in the output image)
-        dx = np.arange(self.size) - self.size / 2 + 0.5
-        dy = np.arange(self.size) - self.size / 2 + 0.5
-
-        # scaling image
-        dx = dx * (item['size'] / self.size)
-        dy = dy * (item['size'] / self.size)
-
-        # mirrored
-        if item['mirrored']:
-            dy = dy[::-1]
-
-        # applying rotation (derived from the cos(a + b) and sin(a + b) equations)
-        XX, YY = np.meshgrid(dx, dy, indexing='ij')
-
-        sin_alpha = np.sin(item['angle'])
-        cos_alpha = np.cos(item['angle'])
-
-        XXA = XX * cos_alpha - YY * sin_alpha
-        YYA = YY * cos_alpha + XX * sin_alpha
-
-        rotated = cxf.interp_matrix(image, item['anchor'][0] + XXA, item['anchor'][1] + YYA)
-        label = int(scan.in_any_roi(item['anchor']))
-
-        # `.unsqueeze(0)` for the `in_channels`
-        return torch.tensor(rotated, dtype=torch.float32).unsqueeze(0), label
+        self.items = []
+        images = []
+        labels = []
+        for branch in dataset:
+            for anchor in cxa.traverse_branch_nodes(branch):
+                item = {
+                    "branch_obj": branch,  # VesselBranch
+                    "anchor": anchor,  # np.ndarray['2', float]
+                    "angle": 0.0,  # float
+                    "size": size,  # int
+                    "mirrored": False,  # bool
+                    # TODO another transformations? background tweaking? noise?
+                }
+                images.append(branch.scan.crop_at(anchor, size))
+                labels.append(int(branch.scan.in_any_roi(anchor)))
+                self.items.append(item)
+        self.images = np.array(images, dtype=np.float32)
+        self.labels = np.array(labels, dtype=np.byte)
+        self.transforms = transforms or ToTensor()
 
     def __len__(self) -> int:
         if self.upsample_positive_examples:
-            return 2 * len(self.negative_examples)
-        return len(self.dataset)
+            return 2 * (self.labels == 0).sum()
+        return len(self.items)
 
-    def __getitem__(self, idx: int) -> tuple[TensorType["in_channels", "height", "width"], int]:
+    def __getitem__(
+        self, idx: int
+    ) -> tuple[TensorType["in_channels", "height", "width"], int]:
         # this ensures that each example will appear at least once in the epoch
-        if idx < len(self.dataset):
-            item = self.dataset[idx]
+        if idx < len(self.items):
+            img = self.images[idx]
+            label = self.labels[idx]
         # for all the rest we sample from the positive examples
         else:
-            item = random.choice(self.positive_examples)
+            label = 1
+            img = random.choice(self.images[self.labels == label])
 
-        if self.train_transform:
-            item = self._transform(item)
-        return self._materialize_item(item)
+        return self.transforms(img), label
 
 
 class ROIClassificationDataModule(pl.LightningDataModule):
-    def __init__(
-            self,
-            cfg: DictConfig
-    ):
+    def __init__(self, cfg: DictConfig):
         super().__init__()
 
         self.cfg = cfg
@@ -157,62 +106,88 @@ class ROIClassificationDataModule(pl.LightningDataModule):
         pass
 
     def setup(self, stage: Optional[str] = None) -> None:
-        if stage is None or stage == 'train':
+        if stage is None or stage == "train":
+            train_transforms = Compose(
+                [
+                    ToTensor(),
+                    RandomHorizontalFlip(p=0.5),
+                    RandomVerticalFlip(p=0.5),
+                    RandomRotation(degrees=180),
+                ]
+            )
             dataset = cx.read_dataset(self.cfg.datamodule.train_dir)
 
-            self.train_dataset = ROIClassificationDataset([
-                branch
-                for scan in dataset
-                for branch in cxa.split_into_branches(scan)
-            ], upsample_positive_examples=self.cfg.datamodule.upsample_positive_examples)
+            self.train_dataset = ROIClassificationDataset(
+                [
+                    branch
+                    for scan in dataset
+                    for branch in cxa.split_into_branches(scan)
+                ],
+                upsample_positive_examples=self.cfg.datamodule.upsample_positive_examples,
+                transforms=train_transforms,
+            )
 
-        if stage is None or stage == 'val':
+        if stage is None or stage == "val":
             dataset = cx.read_dataset(self.cfg.datamodule.val_dir)
 
-            self.val_dataset = ROIClassificationDataset([
-                branch
-                for scan in dataset
-                for branch in cxa.split_into_branches(scan)
-            ], train_transform=False)
+            self.val_dataset = ROIClassificationDataset(
+                [
+                    branch
+                    for scan in dataset
+                    for branch in cxa.split_into_branches(scan)
+                ],
+            )
 
-        if stage is None or stage == 'test':
+        if stage is None or stage == "test":
             dataset = cx.read_dataset(self.cfg.datamodule.test_dir)
 
-            self.test_dataset = ROIClassificationDataset([
-                branch
-                for scan in dataset
-                for branch in cxa.split_into_branches(scan)
-            ], train_transform=False)
+            self.test_dataset = ROIClassificationDataset(
+                [
+                    branch
+                    for scan in dataset
+                    for branch in cxa.split_into_branches(scan)
+                ],
+            )
 
-        if stage is None or stage == 'predict':
+        if stage is None or stage == "predict":
             dataset = cx.read_dataset(self.cfg.datamodule.predict_dir)
 
-            self.predict_dataset = ROIClassificationDataset([
-                branch
-                for scan in dataset
-                for branch in cxa.split_into_branches(scan)
-            ], train_transform=False)
+            self.predict_dataset = ROIClassificationDataset(
+                [
+                    branch
+                    for scan in dataset
+                    for branch in cxa.split_into_branches(scan)
+                ],
+            )
 
     def train_dataloader(self) -> TRAIN_DATALOADERS:
-        return DataLoader(self.train_dataset,
-                          batch_size=self.train_batch_size,
-                          shuffle=True,
-                          num_workers=6)
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.train_batch_size,
+            shuffle=True,
+            num_workers=2,
+        )
 
     def val_dataloader(self) -> EVAL_DATALOADERS:
-        return DataLoader(self.val_dataset,
-                          batch_size=self.val_batch_size,
-                          shuffle=False,
-                          num_workers=3)
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.val_batch_size,
+            shuffle=False,
+            num_workers=2,
+        )
 
     def test_dataloader(self) -> EVAL_DATALOADERS:
-        return DataLoader(self.test_dataset,
-                          batch_size=self.test_batch_size,
-                          shuffle=False,
-                          num_workers=3)
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.test_batch_size,
+            shuffle=False,
+            num_workers=2,
+        )
 
     def predict_dataloader(self) -> EVAL_DATALOADERS:
-        return DataLoader(self.predict_dataset,
-                          batch_size=self.predict_batch_size,
-                          shuffle=False,
-                          num_workers=3)
+        return DataLoader(
+            self.predict_dataset,
+            batch_size=self.predict_batch_size,
+            shuffle=False,
+            num_workers=2,
+        )
--- main-ml.py
+++ main-ml.py
@@ -10,12 +10,14 @@ from cadml.datamodules import ROIClassificationDataModule
 from cadml.models import SimpleCNN, ResNet
 from cadml.tasks import ROIClassificationTask
 from cadml.callbacks import MetricsLoggingCallback
-
+import torch
 
 logger = logging.getLogger(__name__)
 
 
-def train(cfg: DictConfig, datamodule: pl.LightningDataModule, task: pl.LightningModule):
+def train(
+    cfg: DictConfig, datamodule: pl.LightningDataModule, task: pl.LightningModule
+):
     datamodule.setup()
 
     # TODO add metrics writing to the checkpoint??
@@ -36,64 +38,74 @@ def train(cfg: DictConfig, datamodule: pl.LightningDataModule, task: pl.Lightnin
         callbacks=[MetricsLoggingCallback(), checkpoint_callback],
         # fast_dev_run=True,
         # overfit_batches=10,
-        # profiler='advanced',
+        # profiler='simple',
     )
     # if no checkpoint_path is passed, then it is None, thus the model will start from the very beginning
     trainer.fit(task, datamodule=datamodule, ckpt_path=cfg.model.checkpoint_path)
-    trainer.test(task, datamodule=datamodule, ckpt_path='best')
+    trainer.test(task, datamodule=datamodule, ckpt_path="best")
 
 
-def evaluate(cfg: DictConfig, datamodule: pl.LightningDataModule, task: pl.LightningModule):
+def evaluate(
+    cfg: DictConfig, datamodule: pl.LightningDataModule, task: pl.LightningModule
+):
     if cfg.model.checkpoint_path is None:
-        raise ValueError('no checkpoint path has been passed')
+        raise ValueError("no checkpoint path has been passed")
 
-    datamodule.setup('test')
+    datamodule.setup("test")
 
     trainer = pl.Trainer(
         accelerator=cfg.task.accelerator,
         devices=cfg.task.devices,
+        # precision=32
     )
     trainer.test(task, datamodule=datamodule, ckpt_path=cfg.model.checkpoint_path)
 
 
-def inference(cfg: DictConfig, datamodule: pl.LightningDataModule, task: pl.LightningModule):
-    datamodule.setup('predict')
+def inference(
+    cfg: DictConfig, datamodule: pl.LightningDataModule, task: pl.LightningModule
+):
+    datamodule.setup("predict")
     raise NotImplementedError()
 
 
-@hydra.main(config_path='cadml/conf', config_name='config', version_base=None)
+@hydra.main(config_path="cadml/conf", config_name="config", version_base=None)
 def main(cfg: DictConfig):
     print(OmegaConf.to_yaml(cfg))
 
     pl.seed_everything(cfg.seed)
 
     # FIXME refactor this?
-    if cfg.datamodule.name == 'roi-classification':
+    if cfg.datamodule.name == "roi-classification":
         datamodule = ROIClassificationDataModule(cfg)
     else:
-        raise ValueError('unknown datamodule, can be either `roi-classification` or ...')
+        raise ValueError(
+            "unknown datamodule, can be either `roi-classification` or ..."
+        )
 
-    if cfg.model.name == 'simple-cnn':
+    if cfg.model.name == "simple-cnn":
         model = SimpleCNN(cfg)
-    elif cfg.model.name == 'resnet':
+    elif cfg.model.name == "resnet":
         model = ResNet(cfg)
     else:
-        raise ValueError('unknown model, can be either `simple-cnn` or ...')
+        raise ValueError("unknown model, can be either `simple-cnn` or ...")
 
-    if cfg.task.name == 'roi-classification':
+    if cfg.task.name == "roi-classification":
         task = ROIClassificationTask(cfg, model)
     else:
-        raise ValueError('unknown task, can be either `roi-classification` or ...')
+        raise ValueError("unknown task, can be either `roi-classification` or ...")
 
-    if cfg.stage == 'train':
+    if cfg.stage == "train":
         train(cfg, datamodule, task)
-    elif cfg.stage == 'evaluate':
+    elif cfg.stage == "evaluate":
         evaluate(cfg, datamodule, task)
-    elif cfg.stage == 'inference':
+    elif cfg.stage == "inference":
         inference(cfg, datamodule, task)
     else:
-        raise ValueError('unknown stage, can be either `train`, `evaluate` or `inference`')
+        raise ValueError(
+            "unknown stage, can be either `train`, `evaluate` or `inference`"
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    torch.set_default_dtype(torch.float32)
     main()
